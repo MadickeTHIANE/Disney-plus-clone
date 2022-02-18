6348c838d1081d94fa591cb4c797a76794c653fa
madicke <madicke.thiane@gmail.com>
2022-02-18 15:49:59 +0100

    Création du container du header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 function Header() {
-  return <div>Header</div>;
+  return <Container>Header</Container>;
 }
 
 export default Header;
+
+const Container = styled.div`
+  height: 70px;
+  background: #090b13;
+`;
