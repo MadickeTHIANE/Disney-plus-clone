08aef0d28e9bb110e674e9d419906a465b4f5645
madicke <madicke.thiane@gmail.com>
2022-02-18 16:54:48 +0100

    Style liens et images

--- src/components/Header.js
+++ src/components/Header.js
@@ -41,6 +41,38 @@ export default Header;
 const Nav = styled.nav`
   height: 70px;
   background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 20px;
+  align-items.center;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    color: white;
+    text-decoration: none;
+    cursor: pointer;
+
+    img {
+      height: 20px;
+    }
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+     
+    }
+  }
 `;
 
 const UserImg = styled.img`
